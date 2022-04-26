42f401bcfc2f399a8491ada4fe4f34a29b8c1be4
Michael <michael.linder2000@gmail.com>
2022-04-26 15:52:56 +0200

    backup: nothing works atm

--- Prjct/motor_controller.c
+++ Prjct/motor_controller.c
@@ -14,19 +14,31 @@ static THD_FUNCTION(MotorController, arg) {
 
     systime_t time;
 
-    int16_t speed = 100;
+    int16_t speed_left = 0;
+    int16_t speed_right = 0;
+    int16_t lr_error = 0;
 
     while(1){
         time = chVTGetSystemTime();
 
         // get left-right error
-        int lr_error = get_left_right_error();
+        lr_error = get_left_right_error();
 
+        //debug code
+        speed_left = lr_error;
+        speed_right = lr_error;
+
+
+        // check that the speed is within the allowed limits
+        if(speed_left > MOTOR_SPEED_LIMIT){speed_left = MOTOR_SPEED_LIMIT;}
+        else if(speed_left < -MOTOR_SPEED_LIMIT){speed_left = -MOTOR_SPEED_LIMIT;}
+        if(speed_right > MOTOR_SPEED_LIMIT){speed_right = MOTOR_SPEED_LIMIT;}
+        else if(speed_right < -MOTOR_SPEED_LIMIT){speed_right = -MOTOR_SPEED_LIMIT;}
 
 
         //applies the speed from the PI regulator
-		// right_motor_set_speed(speed);
-		// left_motor_set_speed(speed);
+		right_motor_set_speed(speed_right);
+		left_motor_set_speed(speed_left);
 
         //100Hz
         chThdSleepUntilWindowed(time, time + MS2ST(10));
